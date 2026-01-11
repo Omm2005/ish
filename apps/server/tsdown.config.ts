2bde7567db9624b2b2f757b20ab20aa4a80b4a39
Om Shah <shahom0306@gmail.com>
2026-01-10 19:51:11 -0800

    ERROR SERVER LOG 2

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: "./src/index.ts",
-  format: "esm",
-  outDir: "./dist",
+  entry: ["src/index.ts"],
+  format: ["esm"],
   clean: true,
+  // Bundle everything - don't externalize anything
   external: [],
   noExternal: [
     "@ish/auth",
-    "@ish/db",
+    "@ish/db", 
     "@ish/env",
-    // Add other @ish/* packages here
+    // Add any other @ish/* workspace packages you're using
   ],
   bundle: true,
+  minify: false,
+  dts: false,
 });
